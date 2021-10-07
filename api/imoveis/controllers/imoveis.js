dfdc5f71daf2268eb9d1856a795793e3efcc30a4
Diego Mossmann <diegomossmann@Mac-Pro-de-Diego.local>
2021-10-07 13:14:53 -0300

    Total items imoveis

--- api/imoveis/controllers/imoveis.js
+++ api/imoveis/controllers/imoveis.js
@@ -7,6 +7,31 @@ module.exports = {
    * @return {Object}
    */
 
+  async find(ctx) {
+    let entities;
+
+    let totalImoveis;
+    const allItems = await strapi.services.imoveis.find();
+    totalImoveis = allItems.length;
+
+    if (ctx.query._q) {
+      const data = await strapi.services.imoveis.search(ctx.query);
+      entities = data;
+    } else {
+      const data = await strapi.services.imoveis.find(ctx.query);
+      entities = data;
+    }
+
+    const imoveis = entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.imoveis })
+    );
+
+    return {
+      totalImoveis,
+      imoveis,
+    };
+  },
+
   async findOne(ctx) {
     const { slug } = ctx.params;
 
