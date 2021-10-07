197ab923da9bbbb86d1da41f6dc875e2513b197e
Diego Mossmann <diegomossmann@Mac-Pro-de-Diego.local>
2021-10-07 16:32:50 -0300

    Relation Tipo and Cidade

--- api/imoveis/controllers/imoveis.js
+++ api/imoveis/controllers/imoveis.js
@@ -38,4 +38,48 @@ module.exports = {
     const entity = await strapi.services.imoveis.findOne({ slug });
     return sanitizeEntity(entity, { model: strapi.models.imoveis });
   },
+
+  count(ctx) {
+    if (ctx.query._q) {
+      return strapi.services.restaurant.countSearch(ctx.query);
+    }
+    return strapi.services.restaurant.count(ctx.query);
+  },
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.restaurant.create(data, { files });
+    } else {
+      entity = await strapi.services.restaurant.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.restaurant.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.restaurant.update(
+        { id },
+        ctx.request.body
+      );
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.restaurant.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
 };
